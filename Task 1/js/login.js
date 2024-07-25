2fb3cb23c39a08bbcabe1e23a3aff8dec8cbc35c
MADHAVAN-BE-2003 <117741972+MADHAVAN-BE-2003@users.noreply.github.com>
2024-07-25 13:14:11 +0530

    Task 1

---
+++ Task 1/js/login.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  if (localStorage.getItem("rememberMe") === "true") {
+    $("#username").val(localStorage.getItem("username"));
+    $("#password").val(localStorage.getItem("password"));
+    $("#form-checkbox").prop("checked", true);
+  }
+
+  $("#loginForm").on("submit", function (event) {
+    event.preventDefault();
+
+    var formData = $(this).serialize();
+    var rememberMe = $("#form-checkbox").is(":checked");
+
+    loginUser(formData, rememberMe);
+  });
+});
+
+function loginUser(formData, rememberMe) {
+  console.log(formData);
+  $.ajax({
+    type: "POST",
+    url: "php/login.php",
+    dataType: "json",
+    data: formData,
+    success: function (response) {
+      console.log(response);
+
+      var res;
+      try {
+        res = response;
+      } catch (error) {
+        console.error("Failed:", error);
+        return;
+      }
+
+      if (res.status === "success") {
+        localStorage.setItem("email", res.email);
+        if (rememberMe) {
+          localStorage.setItem("session_token", res.token);
+          localStorage.setItem("rememberMe", true);
+          localStorage.setItem("username", $("#username").val());
+          localStorage.setItem("password", $("#password").val());
+        } else {
+          sessionStorage.setItem("session_token", res.token);
+          localStorage.removeItem("rememberMe");
+          localStorage.removeItem("username");
+          localStorage.removeItem("password");
+        }
+        window.location.href = "profile.html";
+      } else {
+        alert(res.message);
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("AJAX Error:", status, error);
+      console.error("Response Text:", xhr.responseText);
+      alert(
+        "An error occurred while processing your request. Please try again."
+      );
+    },
+  });
+}
---
+++ Task 1/js/register.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $("#registerForm").on("submit", function (event) {
+    let formData = $(this).serialize();
+    registerFormSubmit(event, formData);
+  });
+});
+
+let registerFormSubmit = (event, formData) => {
+  console.log(formData);
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "php/register.php",
+    data: formData,
+    success: function (response) {
+      console.log(response);
+      var res = response;
+      alert(res.message);
+      if (res.status === "success") {
+        window.location.href = "login.html";
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("AJAX Error:", status, error);
+      console.error("Response Text:", xhr.responseText);
+      alert(
+        "An error occurred while processing your request. Please try again."
+      );
+    },
+  });
+};
