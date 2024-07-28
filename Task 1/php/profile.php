a7463953878eb107e8254d257f24eb246600067c
MADHAVAN-BE-2003 <117741972+MADHAVAN-BE-2003@users.noreply.github.com>
2024-07-28 15:51:55 +0530

    Task 1

--- Task 1/php/profile.php
+++ Task 1/php/profile.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: ngrok-skip-browser-warning, Content-Type");
 
 require '../vendor/autoload.php';
 
